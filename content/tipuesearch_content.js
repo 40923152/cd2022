bb964e5ee5aea3df094f00bef0092732516c8a8e
40923152 <40923152@gm.nfu.edu.tw>
2022-04-06 10:56:56 +0800

    shit

--- content/tipuesearch_content.js
+++ content/tipuesearch_content.js
@@ -1,1 +1,1 @@
-var tipuesearch = {"pages": [{'title': 'About', 'text': 'cmsimde_site', 'tags': '', 'url': 'About.html'}]};
+var tipuesearch = {"pages": [{'title': 'About', 'text': '1.Hi', 'tags': '', 'url': 'About.html'}]};
